9146c751686e5d3f918ae94fd7c58e1e7b8cd61d
Sabah <sabahp@umich.edu>
2019-04-17 15:11:41 -0400

    timing of script added, foreign key works

--- scrape_data_populate_db.py
+++ scrape_data_populate_db.py
@@ -8,6 +8,7 @@ import requests, json
 from advanced_expiry_caching import Cache
 from datetime import date, timedelta, datetime
 from main_app import Delegacion, Calidad, session
+import time
 
 ######################### Scraping Functions ####################################
 FILENAME = 'cache.json'
@@ -18,11 +19,11 @@ def scrape_pg(url):
     if program_cache.get(url):
         # print('inside try')
         data = program_cache.get(url)                                           # tries to get data from cache
-        print('got fr cache')
+        # print('got fr cache')
     else:                                                                       # if url with the given date periods isn't in the cache then you request the data again
         # print('inside except')
         data = requests.get(url).text
-        print('made request')
+        # print('made request')
         program_cache.set(url, data, expire_in_days = 365)                      # place data into cache for later
         # print('done putting in cache')
     return data
@@ -42,13 +43,15 @@ def get_urls(data, urls_lst=[]):
     return urls_lst
 
 def get_tr_tags(url):
+    ''''''
     data = scrape_pg(url)
     soup = BeautifulSoup(data, "html.parser")
     return soup.findChildren('tr', {"class": "trLink"})
 
 ############################## DB Functions #####################################
 
-def add_delegacion_to_db(name):
+def get_or_create_delegacion_db(name):
+    ''''''
     delegacion = Delegacion.query.filter_by(name=name).first()
     if delegacion:
         return delegacion
@@ -58,20 +61,23 @@ def add_delegacion_to_db(name):
         session.commit()
     return delegacion
 
-def add_calidad_to_db(date, neighborhood, street, num_samples, readings, average, num_no_cl, num_low_cl, num_rule_cl, num_excess_cl, url):
+def get_or_create_calidad_db(date, neighborhood, street, num_samples, readings, average, num_no_cl, num_low_cl, num_rule_cl, num_excess_cl, url, delegacion):
+    ''''''
     calidad = Calidad.query.filter_by(date = date, street = street, average = average).first()
     if calidad:
         return calidad
     else:
-        calidad = Calidad(date = date, neighborhood = neighborhood, street = street, num_samples = num_samples, readings = readings, average = average, num_no_cl = num_no_cl, num_low_cl = num_low_cl, num_rule_cl = num_rule_cl, num_excess_cl = num_excess_cl, url = url)
+        delegacion = get_or_create_delegacion_db(delegacion)
+        calidad = Calidad(date = date, neighborhood = neighborhood, street = street, num_samples = num_samples, readings = readings, average = average, num_no_cl = num_no_cl, num_low_cl = num_low_cl, num_rule_cl = num_rule_cl, num_excess_cl = num_excess_cl, url = url, delegacion = delegacion)
         session.add(calidad)
         session.commit()
     return calidad
 
 ######################## Scrape Data and Put into DB ############################
+start_time = time.time()
 
-start_date = date(2019,1,1)
-end_date = date(2019,1,31)
+start_date = date(2018,11,1)
+end_date = date(2018,11,30)
 delta = end_date - start_date
 
 for i in range(delta.days + 1):
@@ -84,7 +90,7 @@ for i in range(delta.days + 1):
 
     colonias = []
     for delegacion, url in delegaciones:
-        add_delegacion_to_db(name=delegacion)
+        # get_or_create_delegacion_db(name=delegacion)
         tr_tags = get_tr_tags(url)
         for tr_tag in tr_tags:
             if tr_tag.findChild('a', {"class": "cargaCont"}, href=True):
@@ -103,15 +109,18 @@ for i in range(delta.days + 1):
                 cruce = (td.text.strip())
                 cruces.append([delegacion, colonia, cruce, url])
 
-
     for delegacion, colonia, cruce, url in cruces:
             data_pt = []
             tr_tags = get_tr_tags(url)
             for tr_tag in tr_tags :
                 for pt in tr_tag.findChildren('td')[1:]:
+                    # if len(tr_tag.findChildren('td')) !=8:
+                    #     print(len(tr_tag.findChildren('td')))
                     if pt.text.strip()=='':
                         data_pt.append(0)
                     else:
                         data_pt.append(pt.text.strip())
+                get_or_create_calidad_db (date = datetime.strptime(day,'%Y/%m/%d').date(), neighborhood = colonia, street = cruce, num_samples = int(data_pt[0]), readings = int(data_pt[1]), average = float(data_pt[2]), num_no_cl = int(data_pt[3]), num_low_cl = int(data_pt[4]), num_rule_cl = int(data_pt[5]), num_excess_cl = int(data_pt[6]), url = url , delegacion = delegacion)
                 data_pt = []
-                add_calidad_to_db (date = datetime.strptime(day,'%Y/%m/%d').date(), neighborhood = colonia, street = cruce, num_samples = int(data_pt[0]), readings = int(data_pt[1]), average = float(data_pt[2]), num_no_cl = int(data_pt[3]), num_low_cl = int(data_pt[4]), num_rule_cl = int(data_pt[5]), num_excess_cl = int(data_pt[6]), url = url)
+
+print("--- %s seconds ---" % (time.time() - start_time))
